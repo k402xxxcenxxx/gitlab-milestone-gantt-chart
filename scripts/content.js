79fc5228a058c3ad7fdc1721067e6aa8feb6cd2f
k402xxxcenxxx <k402xxxcenxxx@gmail.com>
2023-02-17 23:03:04 +0800

    Fix: image include problem.

--- scripts/content.js
+++ scripts/content.js
@@ -3,49 +3,76 @@ function parse_issues_items(issue_page) {
     const issue_list = issue_page.querySelectorAll("ul.issues-list > li")
 
     issue_list.forEach(
-        function(issue, currentIndex, listObj) {
-            const issue_item = {
-                "id": `issue-${currentIndex}`,
-                "tooltip": null
-            }
-
+        async function(issue, currentIndex, listObj) {
             const title = issue.querySelector("div > div.issuable-main-info > div.issue-title.title > span > a")
-            const start_date = issue.querySelector("div > div.issuable-main-info > div.issuable-info > span.issuable-authored > time")
-            const due_date = issue.querySelector("div > div.issuable-main-info > div.issuable-info > span.issuable-due-date")
+            const due_date_string = issue.querySelector("div > div.issuable-main-info > div.issuable-info > span.issuable-due-date")
 
-            if (title) {
-                issue_item.name = title.innerText
-            }
+            if (title && due_date_string) {
 
-            if (start_date && due_date) {
-                issue_item.values = [
-                    {
-                        from: start_date.innerText,
-                        to: due_date.innerText,
-                        label: title.innerText,
+                const issue_url = title.getAttribute('href')
+                const get_issue_response = fetch(issue_url, {
+                    headers: {
+                        'Accept': 'application/json',
                     }
-                ]
-            }
+                })
+                const issue_page_content = get_issue_response.json()
 
-            issue_items.push(issue_item);
+                // parse issue items
+                const issue_description = issue_page_content["description"]
+                const start_date_re = new RegExp(/^\/start_date\ *(\d{4}\/(0?[1-9]|1[012])\/(0?[1-9]|[12][0-9]|3[01])*)/g);
+                const start_date_pattern = start_date_re.exec(issue_description)
+
+                if (start_date_pattern) {
+                    const start_date = start_date_pattern[1]
+                    const due_date = new Date(due_date_string.innerText)
+                    var issue_item = {
+                        "id": `issue-${currentIndex}`,
+                        "name": title.innerText,
+                        "tooltip": null,
+                        "values": [
+                            {
+                                from: start_date,
+                                to: due_date.toLocaleDateString('zh-Hans-CN', { year: 'numeric', month: '2-digit', day: '2-digit' }),
+                                label: title.innerText,
+                            }
+                        ]
+                    }
+                    issue_items.push(issue_item);
+                }
+            }
         }
     );
+    console.log(issue_items)
     return issue_items;
 }
 
-function build_gantt_chart(chart_id, task_list) {
+function build_gantt_chart(chart_id, issue_url) {
+
+    const task_list = get_issue_items(issue_url);
+    console.log(task_list)
+
     $(`#${chart_id}`).gantt({
         source: task_list,
         scale: "weeks",
         minScale: "days",
         navigate: "scroll"
     });
-
-    $(".fn-gantt .dataPanel").css("background-image", `url(${chrome.runtime.getURL("images/grid.png")})`)
-    $(".fn-gantt .navigate .nav-slider-button").css("background", `url(${chrome.runtime.getURL("images/slider_handle.png")}) center center no-repeat;`)
-    $(".fn-gantt .nav-link").css("background", `#595959 url(${chrome.runtime.getURL("images/icon_sprite.png")}) !important;`)
 }
 
+async function get_issue_items(issue_url) {
+    const get_issue_list_response = await fetch(issue_url, {
+        headers: {
+            'Accept': 'application/json',
+        }
+    })
+    const issue_list_content = get_issue_list_response.json();
+    const parser = new DOMParser();
+    const issue_page = parser.parseFromString(issue_list_content.html, 'text/html');
+
+    // parse issue items
+    const issue_items = parse_issues_items(issue_page)
+    return issue_items
+}
 
 $(function() {
 
@@ -59,26 +86,14 @@ $(function() {
             function (milestone, currentIndex, listObj) {
                 const issue_link = milestone.querySelector("div > div.milestone-progress > a:nth-child(2)")
                 const issue_url = issue_link.getAttribute('href');
-                fetch(issue_url, {
-                    headers: {
-                        'Accept': 'application/json',
-                    }
-                }).then(r => r.json()).then(issue_page_content => {
-                    const parser = new DOMParser();
-                    const issue_page = parser.parseFromString(issue_page_content.html, 'text/html');
+             
+                // insert the gantt after each milestone
+                const gantt_object = document.createElement("div");
+                gantt_object.setAttribute("id", `gantt-chart-${currentIndex}`);
+                milestone.insertAdjacentElement("afterend", gantt_object);
 
-                    // parse issue items
-                    const issue_items = parse_issues_items(issue_page)
-
-                    // insert the gantt after each milestone
-                    const gantt_object = document.createElement("div");
-                    gantt_object.setAttribute("id", `gantt-chart-${currentIndex}`);
-                    // gantt_object.textContent = issue_items.toString()
-                    milestone.insertAdjacentElement("afterend", gantt_object);
-
-                    // draw and build the gantt chart object
-                    build_gantt_chart(`gantt-chart-${currentIndex}`, issue_items)
-                })
+                // draw and build the gantt chart object
+                build_gantt_chart(`gantt-chart-${currentIndex}`, issue_url)
             }
         );
     }
