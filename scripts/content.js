1f19c6b89571e4aaf529f876d271550e359df081
k402xxxcenxxx <k402xxxcenxxx@gmail.com>
2023-02-18 22:34:37 +0800

    Fix: async issue(but there will be a loading issue)

--- scripts/content.js
+++ scripts/content.js
@@ -1,58 +1,61 @@
-function parse_issues_items(issue_page) {
-    const issue_items = []
-    const issue_list = issue_page.querySelectorAll("ul.issues-list > li")
+async function _parse_issue_item(issue, currentIndex) {
+    let issue_item = null;
 
-    issue_list.forEach(
-        async function(issue, currentIndex, listObj) {
-            const title = issue.querySelector("div > div.issuable-main-info > div.issue-title.title > span > a")
-            const due_date_string = issue.querySelector("div > div.issuable-main-info > div.issuable-info > span.issuable-due-date")
+    const title = issue.querySelector("div > div.issuable-main-info > div.issue-title.title > span > a")
+    const due_date_string = issue.querySelector("div > div.issuable-main-info > div.issuable-info > span.issuable-due-date")
 
-            if (title && due_date_string) {
+    if (title && due_date_string) {
+        const issue_item_url = title.getAttribute('href')
+        const response = await fetch(issue_item_url, {
+            headers: {
+                'Accept': 'application/json',
+            }
+        })
+        const issue_page_content = await response.json();
 
-                const issue_url = title.getAttribute('href')
-                const get_issue_response = fetch(issue_url, {
-                    headers: {
-                        'Accept': 'application/json',
+        // parse issue items
+        const issue_description = issue_page_content["description"]
+        const start_date_re = new RegExp(/^\/start_date\ *(\d{4}\/(0?[1-9]|1[012])\/(0?[1-9]|[12][0-9]|3[01])*)/g);
+        const start_date_pattern = start_date_re.exec(issue_description)
+
+        if (start_date_pattern) {
+            const start_date = start_date_pattern[1]
+            const due_date = new Date(due_date_string.innerText)
+
+            issue_item = {
+                "id": `issue-${currentIndex}`,
+                "name": title.innerText,
+                "tooltip": null,
+                "values": [
+                    {
+                        from: start_date,
+                        to: due_date.toLocaleDateString('zh-Hans-CN', { year: 'numeric', month: '2-digit', day: '2-digit' }),
+                        label: title.innerText,
                     }
-                })
-                const issue_page_content = get_issue_response.json()
-
-                // parse issue items
-                const issue_description = issue_page_content["description"]
-                const start_date_re = new RegExp(/^\/start_date\ *(\d{4}\/(0?[1-9]|1[012])\/(0?[1-9]|[12][0-9]|3[01])*)/g);
-                const start_date_pattern = start_date_re.exec(issue_description)
-
-                if (start_date_pattern) {
-                    const start_date = start_date_pattern[1]
-                    const due_date = new Date(due_date_string.innerText)
-                    var issue_item = {
-                        "id": `issue-${currentIndex}`,
-                        "name": title.innerText,
-                        "tooltip": null,
-                        "values": [
-                            {
-                                from: start_date,
-                                to: due_date.toLocaleDateString('zh-Hans-CN', { year: 'numeric', month: '2-digit', day: '2-digit' }),
-                                label: title.innerText,
-                            }
-                        ]
-                    }
-                    issue_items.push(issue_item);
-                }
+                ]
             }
         }
-    );
-    console.log(issue_items)
+    }
+    return issue_item;
+}
+
+async function parse_issues_items(issue_page) {
+    let issue_items = []
+    const issue_list = issue_page.querySelectorAll("ul.issues-list > li")
+
+    for (let index = 0; index < issue_list.length; index++) {
+        const issue_item = await _parse_issue_item(issue_list[index], index);
+        if (issue_item) {
+            issue_items.push(issue_item);
+        }
+    }
+
     return issue_items;
 }
 
-function build_gantt_chart(chart_id, issue_url) {
-
-    const task_list = get_issue_items(issue_url);
-    console.log(task_list)
-
-    $(`#${chart_id}`).gantt({
-        source: task_list,
+function build_gantt_chart(id, gantt_items) {
+    $(`#${id}`).gantt({
+        source: gantt_items,
         scale: "weeks",
         minScale: "days",
         navigate: "scroll"
@@ -60,42 +63,49 @@ function build_gantt_chart(chart_id, issue_url) {
 }
 
 async function get_issue_items(issue_url) {
-    const get_issue_list_response = await fetch(issue_url, {
+    let issue_items = [];
+
+    const response = await fetch(issue_url, {
         headers: {
             'Accept': 'application/json',
         }
     })
-    const issue_list_content = get_issue_list_response.json();
+    const issue_list_content = await response.json();
     const parser = new DOMParser();
     const issue_page = parser.parseFromString(issue_list_content.html, 'text/html');
 
     // parse issue items
-    const issue_items = parse_issues_items(issue_page)
+    issue_items = await parse_issues_items(issue_page)
     return issue_items
 }
 
+async function insert_gantt_chart(id, position, issue_url) {
+    // create DOM object
+    const gantt_object = document.createElement("div");
+    gantt_object.setAttribute("id", id);
+    position.insertAdjacentElement("afterend", gantt_object);
+
+    // collect gantt items
+    const gantt_items = await get_issue_items(issue_url);
+
+    // build the gantt chart object
+    build_gantt_chart(id, gantt_items)
+}
+
 $(function() {
-
-    "use strict";
-
+    // get all milestone list in this page
     const milestone_list = document.querySelectorAll("#content-body > div.milestones > ul > li");
-
-    // `document.querySelectorAll` may return empty NodeList if the selector doesn't match anything.
     if (milestone_list.length > 0) {
+
+        // create gantt chart for each milestone
         milestone_list.forEach(
             function (milestone, currentIndex, listObj) {
                 const issue_link = milestone.querySelector("div > div.milestone-progress > a:nth-child(2)")
                 const issue_url = issue_link.getAttribute('href');
-             
-                // insert the gantt after each milestone
-                const gantt_object = document.createElement("div");
-                gantt_object.setAttribute("id", `gantt-chart-${currentIndex}`);
-                milestone.insertAdjacentElement("afterend", gantt_object);
 
-                // draw and build the gantt chart object
-                build_gantt_chart(`gantt-chart-${currentIndex}`, issue_url)
+                // insert the gantt after each milestone
+                insert_gantt_chart(`gantt-chart-${currentIndex}`, milestone, issue_url);
             }
         );
     }
-
 });
