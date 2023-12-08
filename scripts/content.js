058eeda62b2e40d83d55f1764790052ba6e0a64d
k402xxxcenxxx <k402xxxcenxxx@gmail.com>
2023-12-09 01:44:44 +0800

    Update content.js to use graphql

--- scripts/content.js
+++ scripts/content.js
@@ -1,59 +1,56 @@
-async function _parse_issue_item(issue, currentIndex) {
-    let issue_item = null;
+function build_gantt_chart(id, gantt_data) {
+    let milestone_dict = {}
+    gantt_data.project.milestones.nodes.forEach(
+        function (milestone, currentIndex, listObj) {
+            milestone_dict[milestone.id] = {
+                title: milestone.title,
+                issue_list: []
+            };
+        }
+    );
 
-    const title = issue.querySelector("div > div.issuable-main-info > div.issue-title.title > span > a")
-    const due_date_string = issue.querySelector("div > div.issuable-main-info > div.issuable-info > span.issuable-due-date")
-
-    if (title && due_date_string) {
-        const issue_item_url = title.getAttribute('href')
-        const response = await fetch(issue_item_url, {
-            headers: {
-                'Accept': 'application/json',
+    gantt_data.project.issues.nodes.forEach(
+        function (issue, currentIndex, listObj) {
+            // parse start date
+            const start_date_re = new RegExp(/^\/start_date\ *(\d{4}\/(0?[1-9]|1[012])\/(0?[1-9]|[12][0-9]|3[01])*)/g);
+            const start_date_pattern = start_date_re.exec(issue.description);
+            let start_date = issue.createdAt;
+            if (start_date_pattern) {
+                start_date = start_date_pattern[1];
             }
-        })
-        const issue_page_content = await response.json();
 
-        // parse issue items
-        const issue_description = issue_page_content["description"]
-        const start_date_re = new RegExp(/^\/start_date\ *(\d{4}\/(0?[1-9]|1[012])\/(0?[1-9]|[12][0-9]|3[01])*)/g);
-        const start_date_pattern = start_date_re.exec(issue_description)
-
-        if (start_date_pattern) {
-            const start_date = start_date_pattern[1]
-            const due_date = new Date(due_date_string.innerText)
-
-            issue_item = {
-                id: `issue-${currentIndex}`,
-                desc: title.innerText,
+            milestone_dict[issue.milestone.id].issue_list.push({
+                id: issue.id,
+                desc: issue.description,
                 values: [
                     {
                         from: start_date,
-                        to: due_date.toLocaleDateString('zh-Hans-CN', { year: 'numeric', month: '2-digit', day: '2-digit' }),
-                        label: title.innerText,
-                        desc: issue_description,
-                        dataObj: issue_item_url
+                        to: issue.dueDate,
+                        label: issue.title,
+                        desc: issue.description,
+                        dataObj: issue.webUrl
                     }
                 ]
+            });
+        }
+    );
+
+    gantt_items = []
+    Object.keys(milestone_dict).forEach(
+        function (milestone_id, currentIndex, listObj) {
+            if (milestone_dict[milestone_id].issue_list.length > 0) {
+                milestone_dict[milestone_id].issue_list[0].name = milestone_dict[milestone_id].title;
+            } else {
+                milestone_dict[milestone_id].issue_list = [{
+                    id: milestone_id,
+                    name: milestone_dict[milestone_id].title,
+                    values: []
+                }]
             }
+            gantt_items.push(...milestone_dict[milestone_id].issue_list);
         }
-    }
-    return issue_item;
-}
+    );
 
-async function parse_issues_items(issue_page, prefix) {
-    let issue_items = []
-    const issue_list = issue_page.querySelectorAll("ul.issues-list > li");
-
-    for (let index = 0; index < issue_list.length; index++) {
-        const issue_item = await _parse_issue_item(issue_list[index], `${prefix}-${index}`);
-        if (issue_item) {
-            issue_items.push(issue_item);
-        }
-    }
-    return issue_items
-}
-
-function build_gantt_chart(id, gantt_items) {
     $(`#${id}`).gantt({
         source: gantt_items,
         scale: "weeks",
@@ -66,70 +63,55 @@ function build_gantt_chart(id, gantt_items) {
     });
 }
 
-async function get_gantt_items(milestone_url_list) {
-    let gantt_items = [];
-    for (let index = 0; index < milestone_url_list.length; index++) {
-        const milestone_item = milestone_url_list[index];
 
-        const response = await fetch(milestone_item.url, {
-            headers: {
-                'Accept': 'application/json',
-            }
-        })
-        const issue_list_content = await response.json();
-        const parser = new DOMParser();
-        const issue_page = parser.parseFromString(issue_list_content.html, 'text/html');
-
-        // parse issue items
-        let issue_items = await parse_issues_items(issue_page, index);
-        if (issue_items.length > 0) {
-            issue_items[0].name = milestone_item.title;
-        } else {
-            issue_items = [{
-                id: `issue-${index}`,
-                name: milestone_item.title,
-                values: []
-            }]
-        }
-        gantt_items.push(...issue_items);
-    }
-    return gantt_items
-}
-
-async function insert_gantt_chart(id, position, milestone_url_list) {
+async function insert_gantt_chart(id, position, gantt_items) {
     // create DOM object
     const gantt_object = document.createElement("div");
     gantt_object.setAttribute("id", id);
     position.insertAdjacentElement("beforebegin", gantt_object);
 
-    // collect gantt items
-    const gantt_items = await get_gantt_items(milestone_url_list);
-
     // build the gantt chart object
-    build_gantt_chart(id, gantt_items)
+    build_gantt_chart(id, gantt_items);
 }
 
 $(function() {
-    // get all milestone list in this page
-    const milestone_list = document.querySelectorAll("#content-body > div.milestones > ul > li");
-    if (milestone_list.length > 0) {
-
-        milestone_url_list = []
-
-        // create gantt chart for each milestone
-        milestone_list.forEach(
-            function (milestone, currentIndex, listObj) {
-                const milestone_link = milestone.querySelector("div > div.milestone-progress > a:nth-child(2)")
-                const milestone_url = milestone_link.getAttribute('href');
-                const milestone_title = milestone.querySelector("div > div > div:nth-child(1) > strong > a").innerText;
-                milestone_url_list.push({
-                    title: milestone_title,
-                    url: milestone_url
-                })
-            }
-        );
-
-        // insert the gantt on the top
-        insert_gantt_chart(`gantt-chart`, document.querySelector("#content-body > div.milestones"), milestone_url_list);
-    }
+    const current_url = window.location.href;
+    const project_full_path = current_url.split('/').slice(3, 5).join('/');
+    console.log(project_full_path);
+    fetch('https://gitlab.com/api/graphql', {
+        method: 'POST',
+        headers: {
+            "content-type": "application/json"
+        },
+        body: JSON.stringify({
+            query: `
+                query {
+                    project(fullPath: "${project_full_path}") {
+                      milestones {
+                        nodes {
+                          id
+                          title
+                        }
+                      }
+                      issues {
+                        nodes {
+                          milestone {
+                            id
+                          }
+                          description
+                          title
+                          createdAt
+                          dueDate
+                          webUrl
+                        }
+                      }
+                    }
+                }
+            `
+        })
+    })
+    .then(res => res.json())
+    .then(res => {
+        insert_gantt_chart(`gantt-chart`, document.querySelector("#content-body > div.milestones"), res.data);
+    });
 });
